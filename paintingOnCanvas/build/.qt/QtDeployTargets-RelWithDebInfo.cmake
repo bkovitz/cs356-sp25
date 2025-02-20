35d88cf5c2a2da3c4a65a9e8bfcaa0cd64a01d8f
Tommie Moore <tm276@humboldt.edu>
2025-02-20 18:55:02 -0500

    setup branch for conway's game of life

---
+++ paintingOnCanvas/build/.qt/QtDeployTargets-RelWithDebInfo.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_myproject_FILE C:/Users/mtomm/Documents/home/class/cs_356/cs356-sp25/paintingOnCanvas/build/RelWithDebInfo/myproject.exe)
+set(__QT_DEPLOY_TARGET_myproject_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_myproject_RUNTIME_DLLS C:/Q2t/6.8.2/msvc2022_64/bin/Qt6Widgets.dll;C:/Q2t/6.8.2/msvc2022_64/bin/Qt6Gui.dll;C:/Q2t/6.8.2/msvc2022_64/bin/Qt6Core.dll)
