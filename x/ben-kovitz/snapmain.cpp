46fa12d5ea212c94295aa153a97edadef2e18987
Ben Kovitz <bkovitz@gmail.com>
2025-02-27 01:47:15 -0800

    snapHint as a little red dot

--- x/ben-kovitz/snapmain.cpp
+++ x/ben-kovitz/snapmain.cpp
@@ -3,9 +3,11 @@
 #include <QGraphicsView>
 #include <QApplication>
 #include <QGraphicsRectItem>
+#include <QGraphicsEllipseItem>
 #include <QGraphicsSceneMouseEvent>
 
 class Block : public QGraphicsRectItem {
+  QAbstractGraphicsShapeItem* snapHint = nullptr;
 public:
   Block(QGraphicsItem* parent = nullptr) : QGraphicsRectItem(parent) {
     setRect(0, 0, 100, 100);
@@ -13,11 +15,39 @@ public:
     setFlag(QGraphicsItem::ItemIsMovable);
   }
 
+  void mouseMoveEvent(QGraphicsSceneMouseEvent* event) override {
+    QGraphicsRectItem::mouseMoveEvent(event);
+    //setPos(nearestSnapPoint(event));
+    addSnapHint(event);
+    // auto snapPoint = nearestSnapPoint(event);
+    // scene()->addItem(new QGraphicsEllipseItem(
+    //   snapPoint.x(), snapPoint.y(), 2, 2
+    // ));
+  }
+
+  void addSnapHint(QGraphicsSceneMouseEvent* event) {
+    if (snapHint) {
+      scene()->removeItem(snapHint);
+      delete snapHint;
+      snapHint = nullptr;
+    }
+    auto snapPoint = nearestSnapPoint(event);
+    snapHint = new QGraphicsEllipseItem(snapPoint.x(), snapPoint.y(), 3, 3);
+    snapHint->setBrush(Qt::red);
+    snapHint->setPen(Qt::NoPen);
+    scene()->addItem(snapHint);
+  }
+
   void mouseReleaseEvent(QGraphicsSceneMouseEvent* event) override {
     QGraphicsRectItem::mouseReleaseEvent(event);
     // qreal x = event->scenePos().x();
     // qreal y = event->scenePos().y();
     // setPos(round(x / 100) * 100, round(y / 100) * 100);
+    if (snapHint) {
+      scene()->removeItem(snapHint);
+      delete snapHint;
+      snapHint = nullptr;
+    }
     setPos(nearestSnapPoint(event));
   }
 
