6202401000b2fcf49b5f999fb0037321c083415e
Ben Kovitz <bkovitz@gmail.com>
2025-02-27 01:03:49 -0800

    nearestSnapPoint

--- x/ben-kovitz/snapmain.cpp
+++ x/ben-kovitz/snapmain.cpp
@@ -7,27 +7,35 @@
 
 class Block : public QGraphicsRectItem {
 public:
-    Block(QGraphicsItem* parent = nullptr) : QGraphicsRectItem(parent) {
-        setRect(0, 0, 100, 100);
-        setBrush(Qt::blue);
-        setFlag(QGraphicsItem::ItemIsMovable);
-    }
-    void mouseReleaseEvent(QGraphicsSceneMouseEvent* event) override {
-        QGraphicsRectItem::mouseReleaseEvent(event);
-        qreal x = event->scenePos().x();
-        qreal y = event->scenePos().y();
-        setPos(round(x / 100) * 100, round(y / 100) * 100);
-    }
+  Block(QGraphicsItem* parent = nullptr) : QGraphicsRectItem(parent) {
+    setRect(0, 0, 100, 100);
+    setBrush(Qt::blue);
+    setFlag(QGraphicsItem::ItemIsMovable);
+  }
+
+  void mouseReleaseEvent(QGraphicsSceneMouseEvent* event) override {
+    QGraphicsRectItem::mouseReleaseEvent(event);
+    // qreal x = event->scenePos().x();
+    // qreal y = event->scenePos().y();
+    // setPos(round(x / 100) * 100, round(y / 100) * 100);
+    setPos(nearestSnapPoint(event));
+  }
+
+  QPointF nearestSnapPoint(QGraphicsSceneMouseEvent* event) {
+    qreal x = event->scenePos().x();
+    qreal y = event->scenePos().y();
+    return QPointF(round(x / 100) * 100, round(y / 100) * 100);
+  }
 };
 
 int main(int argc, char** argv) {
-    QApplication app(argc, argv);
-    QGraphicsScene scene(0, 0, 500, 500);
-    
-    Block* block = new Block();
-    scene.addItem(block);
+  QApplication app(argc, argv);
+  QGraphicsScene scene(0, 0, 500, 500);
+  
+  Block* block = new Block();
+  scene.addItem(block);
 
-    QGraphicsView view(&scene);
-    view.show();
-    return app.exec();
+  QGraphicsView view(&scene);
+  view.show();
+  return app.exec();
 }
